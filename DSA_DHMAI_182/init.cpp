6c58747f6c063e1d2a96ee55113c60fe921af6c6
rahmanmehraj182 <116060024+rahmanmehraj182@users.noreply.github.com>
2023-10-09 17:14:12 +0600

    Add files via upload

---
+++ DSA_DHMAI_182/init.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+void printName(){
+    printf("Md Mahamudur Rahman Mehraj\n");
+}
+
+int main(){
+    printf("Md Mahamudur Rahman Mehraj\n");
+    printName();
+    return 0;
+}
